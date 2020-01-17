9bbb93db8ba29bd522f904733b2db9313f8bbe73
Sogo Ogundowole <dowolebolu@gmail.com>
2020-01-17 02:42:06 +0100

    removed db credentials

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///airsynq.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
